a4bfa70d6800dbb062fca0862769ea7928d9ca91
Pavel545 <110118389+Pavel545@users.noreply.github.com>
2022-10-30 15:14:59 +0400

    node

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const { url } = require("inspector");
+const { URL } = require("url");
 const getUsers = require("./modules/users");
 
 const hostname = "127.0.0.1";
@@ -14,32 +15,32 @@ const server = http.createServer((req, res) => {
 
     return;
   }
-  if (req.url === "/?hello=<name>") {
-    res.statusCode = 200;
-    res.statusMessage = "ok";
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello," + req.url+"\n");
+  let url2 = new URL(`http://${hostname}:${port}` + req.url);
+  console.log(url2);
+  console.log(url2.search);
+  if (url2.search.startsWith("?hello")) {
+    const name = url2.searchParams.get("hello");
+    if (name) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end(`Hello, ${name}\n`);
 
-    return;
-  }
-  if ((req.url === "/?hello")|| (req.url === "/?hello=")) {
+      return;
+    }
     res.statusCode = 400;
-    res.statusMessage = "ok";
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Type", "text/plain");
     res.end("Enter a name\n");
-
     return;
   }
-  if (req.url==="/") {
+
+  if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello World\n");
-  return;
-
+    return;
   }
   res.statusCode = 500;
   res.end("\n");
-
 });
 
 server.listen(port, hostname, () => {
